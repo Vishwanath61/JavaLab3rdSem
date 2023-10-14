f74a0bbca16a396c0420809994a0f8714b2b00c7
Vishwanath61 <120776288+Vishwanath61@users.noreply.github.com>
2023-10-14 16:26:38 +0530

    Add files via upload

--- Excercise-6/Exception Handling/NumberFormatException61.java
+++ Excercise-6/Exception Handling/NumberFormatException61.java
@@ -8,22 +8,24 @@ import java.util.*;
  *
  * @author VISHWA
  */
-public class ArithmeticException61
+public class NumberFormatException61  
 {
 public static void main(String[] args) 
     {
         Scanner obj=new Scanner(System.in);
+        String str1,str2;      
         int a,b;
         try
         {
             System.out.print("\nEnter the value of A: ");
-            a=obj.nextInt();
+            str1=obj.next();
             System.out.print("Enter the value of B: ");
-            b=obj.nextInt();
-            int div=a/b;
-            System.out.println("\nThe Division of "+a+" and "+b+" is: "+div);
+            str2=obj.next();
+            a=Integer.parseInt(str1);
+            b=Integer.parseInt(str2);
+            System.out.println("\nThe value of A is "+a+" and B is "+b);
         }
-        catch(ArithmeticException e)
+        catch(NumberFormatException e)
         {
             System.out.println("\n"+e);
         }
